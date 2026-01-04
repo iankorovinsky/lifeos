04fa3cc079733275859eff2dd6be43a96c98884c
Ian Korovinsky <49794230+iankorovinsky@users.noreply.github.com>
2026-01-04 18:49:05 -0500

    Github Workflows (#11)

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -2,6 +2,5 @@ import app from './app';
 import config from './config/config';
 
 app.listen(config.port, () => {
-  // eslint-disable-next-line no-console
   console.log(`Server running on port ${config.port} in ${config.nodeEnv} mode`);
 });
